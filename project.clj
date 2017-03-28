8cd026c5ad24b791f8a23611199a0c1a799ebbfd
Howard M. Lewis Ship <hlship@walmartlabs.com>
2017-03-28 13:32:59 -0700

    Correct a few things in project.clj

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject com.howardlewisship/bgg-graphql-proxy "0.12.0"
+(defproject com.howardlewisship/bgg-graphql-proxy "0.0.1"
   :description "GraphQL interface to BoardGameGeek"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.aviso/logging "0.2.0"]
-                 [com.walmartlabs/lacinia "NEXT"]
+                 [com.walmartlabs/lacinia "0.13.0"]
                  [clj-http "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]
                  [io.pedestal/pedestal.service "0.5.2"]
                  [io.pedestal/pedestal.jetty "0.5.2"]]
-  :repositories [["ereceipts-releases"
-                  "http://dfw-receipts-jenkins.mobile.walmart.com:8081/nexus/content/repositories/releases/"]]
-  :codox {:source-uri ""
+  :codox {:source-uri "https://github.com/hlship/boardgamegeek-graphql-proxy/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
